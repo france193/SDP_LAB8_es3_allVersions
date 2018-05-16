2bfd3c8f7c5ceda9715593fae598987d98caea4d
france193 <france193@DESKTOP-MTGBPT6>
2018-05-16 16:32:54 +0200

    completed

--- Es3_allVersions/Es3_allVersions.cpp
+++ Es3_allVersions/Es3_allVersions.cpp
@@ -224,14 +224,15 @@ VOID readWithFilePointers(INT id, HANDLE hIn) {
 
 	// the displacement in the file is obtained by multiplying the index by the size of a single element
 	position.QuadPart = n * sizeof(s);
-	// set the file pointer
+
+	// set file pointer from FILE_BEGIN
 	SetFilePointerEx(hIn, position, NULL, FILE_BEGIN);
+
 	// and read
 	if (ReadFile(hIn, &s, sizeof(s), &nRead, NULL) && nRead == sizeof(s)) {
 		// read was successful
 		_tprintf(_T("id: %d\treg_number: %ld\tsurname: %10s\tname: %10s\tmark: %d\n"), s.id, s.regNum, s.surname, s.name, s.mark);
-	}
-	else {
+	} else {
 		// some errors in the read (out of bounds?)
 		_ftprintf(stderr, _T("Error in read\n"));
 	}
@@ -245,30 +246,33 @@ VOID writeWithFilePointers(INT id, HANDLE hIn) {
 	LONGLONG n = id - 1;
 	LARGE_INTEGER position;
 	DWORD nWritten;
+
 	// the displacement in the file is obtained by multiplying the index by the size of a single element
 	position.QuadPart = n * sizeof(s);
+
 	SetFilePointerEx(hIn, position, NULL, FILE_BEGIN);
 	_tprintf(_T("You required to write student with id = %d.\nInsert the following data: \"registration_number surname name mark\"\n> "), id);
+	
 	// the id was already entered by the user, no need to ask it again
 	s.id = id;
+	
 	// read a line
 	while (_fgetts(command, CMD_MAX_LEN, stdin)) {
 		// parse the parameters
 		if (_stscanf(command, _T("%ld %s %s %d"), &s.regNum, s.surname, s.name, &s.mark) != 4) {
 			// if parse fails, ask again to input the data
 			_tprintf(_T("Error in the string. The format is the following \"registration_number surname name mark\"\n> "));
-		}
-		else {
+		} else {
 			// if parse is correct, can go on
 			break;
 		}
 	}
+
 	// write the record
 	if (WriteFile(hIn, &s, sizeof(s), &nWritten, NULL) && nWritten == sizeof(s)) {
 		// write was successful
 		_tprintf(_T("Record with id = %d stored\n"), s.id);
-	}
-	else {
+	} else {
 		// some errors in the write
 		_ftprintf(stderr, _T("Error in write\n"));
 	}
@@ -284,18 +288,21 @@ VOID readWithOverlappedDataStructures(INT id, HANDLE hIn) {
 	// the index should be 0-based because at the beginning of the file the student with id = 1 is stored
 	LONGLONG n = id - 1;
 	LARGE_INTEGER FilePos;
+
 	// the displacement in the file is obtained by multiplying the index by the size of a single element
 	FilePos.QuadPart = n * sizeof(s);
+	
 	// copy the displacement inside the OVERLAPPED structure
 	ov.Offset = FilePos.LowPart;
 	ov.OffsetHigh = FilePos.HighPart;
+	
 	_tprintf(_T("You required to read student with id = %d:\n"), id);
+	
 	// the read uses the OVERLAPPED
 	if (ReadFile(hIn, &s, sizeof(s), &nRead, &ov) && nRead == sizeof(s)) {
 		// read was successful
 		_tprintf(_T("id: %d\treg_number: %ld\tsurname: %10s\tname: %10s\tmark: %d\n"), s.id, s.regNum, s.surname, s.name, s.mark);
-	}
-	else {
+	} else {
 		// some errors in the read (out of bounds?)
 		_ftprintf(stderr, _T("Error in read\n"));
 	}
@@ -306,27 +313,42 @@ VOID writeWithOverlappedDataStructures(INT id, HANDLE hIn) {
 	STUDENT s;
 	TCHAR command[CMD_MAX_LEN];
 	DWORD nWritten;
+	// create a "clean" OVERLAPPED structure
 	OVERLAPPED ov = { 0, 0, 0, 0, NULL };
+	// the index should be 0-based because at the beginning of the file the student with id = 1 is stored
 	LONGLONG n = id - 1;
 	LARGE_INTEGER FilePos;
+
+	// the displacement in the file is obtained by multiplying the index by the size of a single element
 	FilePos.QuadPart = n * sizeof(s);
+
+	// copy the displacement inside the OVERLAPPED structure
 	ov.Offset = FilePos.LowPart;
 	ov.OffsetHigh = FilePos.HighPart;
+
 	_tprintf(_T("You required to write student with id = %d.\nInsert the following data: \"registration_number surname name mark\"\n> "), id);
+
+	// the id was already entered by the user, no need to ask it again
 	s.id = id;
+
+	// read a line
 	while (_fgetts(command, CMD_MAX_LEN, stdin)) {
+		// parse the line
 		if (_stscanf(command, _T("%ld %s %s %d"), &s.regNum, s.surname, s.name, &s.mark) != 4) {
+			// if parse fails, ask again to input the data
 			_tprintf(_T("Error in the string. The format is the following \"registration_number surname name mark\"\n> "));
-		}
-		else {
+		} else {
+			// if parse is correct, can go on
 			break;
 		}
 	}
 
+	// write the record
 	if (WriteFile(hIn, &s, sizeof(s), &nWritten, &ov) && nWritten == sizeof(s)) {
+		// write was successful
 		_tprintf(_T("Record with id = %d stored\n"), s.id);
-	}
-	else {
+	} else {
+		// some errors in the write
 		_ftprintf(stderr, _T("Error in write\n"));
 	}
 }
@@ -335,28 +357,39 @@ VOID writeWithOverlappedDataStructures(INT id, HANDLE hIn) {
 // this function is similar to the readOV, but locks the record before reading it
 VOID readWithLocks(INT id, HANDLE hIn) {
 	STUDENT s;
+	// create a "clean" OVERLAPPED structure
 	OVERLAPPED ov = { 0, 0, 0, 0, NULL };
+	// the index should be 0-based because at the beginning of the file the student with id = 1 is stored
 	LONGLONG n = id - 1;
 	LARGE_INTEGER filePos, size;
 	DWORD nRead;
+
+	// the displacement in the file is obtained by multiplying the index by the size of a single element
 	filePos.QuadPart = n * sizeof(s);
+
+	// copy the displacement inside the OVERLAPPED structure
 	ov.Offset = filePos.LowPart;
 	ov.OffsetHigh = filePos.HighPart;
+
+	// fill the variable as QuadPart and then read as lowPart and hightPart to lock the file part
 	size.QuadPart = sizeof(s);
+
 	_tprintf(_T("You required to read student with id = %d:\n"), id);
+
 	// lock the portion of file. The OVERLAPPED is used to specify the starting displacement of the record to be locked
 	// and its size is specified on the 4th and 5th parameter
 	if (!LockFileEx(hIn, 0, 0, size.LowPart, size.HighPart, &ov)) {
 		_ftprintf(stderr, _T("Error locking file portion. Error: \n%s\n"), getErrorMessageAsString(GetLastError()));
 		return;
 	}
+
 	// read the record
 	if (ReadFile(hIn, &s, sizeof(s), &nRead, &ov) && nRead == sizeof(s)) {
 		_tprintf(_T("id: %d\treg_number: %ld\tsurname: %10s\tname: %10s\tmark: %d\n"), s.id, s.regNum, s.surname, s.name, s.mark);
-	}
-	else {
+	} else {
 		_ftprintf(stderr, _T("Error in read\n"));
 	}
+	
 	// unlock the portion of file (arguments similar to the LockFileEx)
 	if (!UnlockFileEx(hIn, 0, size.LowPart, size.HighPart, &ov)) {
 		_ftprintf(stderr, _T("Error unlocking file portion. Error: \n%s\n"), getErrorMessageAsString(GetLastError()));
@@ -365,38 +398,56 @@ VOID readWithLocks(INT id, HANDLE hIn) {
 
 VOID writeWithLocks(INT id, HANDLE hIn) {
 	STUDENT s;
+	// create a "clean" OVERLAPPED structure
 	OVERLAPPED ov = { 0, 0, 0, 0, NULL };
+	// the index should be 0-based because at the beginning of the file the student with id = 1 is stored
 	LONGLONG n = id - 1;
 	LARGE_INTEGER filePos, size;
 	DWORD nWritten;
 	TCHAR command[CMD_MAX_LEN];
+
+	// the displacement in the file is obtained by multiplying the index by the size of a single element
 	filePos.QuadPart = n * sizeof(s);
+
+	// copy the displacement inside the OVERLAPPED structure
 	ov.Offset = filePos.LowPart;
 	ov.OffsetHigh = filePos.HighPart;
+
+	// fill the variable as QuadPart and then read as lowPart and hightPart to lock the file part
 	size.QuadPart = sizeof(s);
+
 	_tprintf(_T("You required to write student with id = %d.\nInsert the following data: \"registration_number surname name mark\"\n> "), id);
+	
+	// the id was already entered by the user, no need to ask it again
 	s.id = id;
+
+	// read a line
 	while (_fgetts(command, CMD_MAX_LEN, stdin)) {
 		if (_stscanf(command, _T("%ld %s %s %d"), &s.regNum, s.surname, s.name, &s.mark) != 4) {
+			// if parse fails, ask again to input the data
 			_tprintf(_T("Error in the string. The format is the following \"registration_number surname name mark\"\n> "));
-		}
-		else {
+		} else {
+			// if parse is correct, can go on
 			break;
 		}
 	}
+
 	// lock the portion of file. The OVERLAPPED is used to specify the starting displacement of the record to be locked
 	// and its size is specified on the 4th and 5th parameter
 	if (!LockFileEx(hIn, LOCKFILE_EXCLUSIVE_LOCK, 0, size.LowPart, size.HighPart, &ov)) {
 		_ftprintf(stderr, _T("Error locking file portion. Error: \n%s\n"), getErrorMessageAsString(GetLastError()));
 		return;
 	}
+
 	// write the record
 	if (WriteFile(hIn, &s, sizeof(s), &nWritten, &ov) && nWritten == sizeof(s)) {
+		// success writing
 		_tprintf(_T("Record with id = %d stored\n"), s.id);
-	}
-	else {
+	} else {
+		// error in writing
 		_ftprintf(stderr, _T("Error in write\n"));
 	}
+
 	// unlock the portion of file
 	if (!UnlockFileEx(hIn, 0, size.LowPart, size.HighPart, &ov)) {
 		_ftprintf(stderr, _T("Error unlocking file portion. Error: \n%s\n"), getErrorMessageAsString(GetLastError()));
